870f68aff6c88bf65d0438a62a29be2dba52bbaf
albab83 <141700905+albab83@users.noreply.github.com>
2024-05-18 20:59:27 +0700

    server

--- back-end/server/index.js
+++ back-end/server/index.js
@@ -4,8 +4,57 @@ const PORT = process.env.PORT || 3001
 
 const app = express()
 
-app.get('/api', (req, res) => {
-   res.json({ message: 'Assalamualaikum Dunia' })
+const mysql = require('mysql2')
+const connection = mysql.createConnection({
+   host: 'localhost',
+   user: 'root',
+   password: '',
+   database: 'parking',
+})
+
+app.use(express.json())
+connection.connect(function (error) {
+   if (!!error) {
+      console.log(error)
+   } else {
+      console.log('Koneksi Berhasil!')
+   }
+})
+
+module.exports = connection
+
+app.post('/pengendara', async (req, res) => {
+   try {
+      const { nama_pengendara, jenis_kendaraan, no_kendaraan, jam_masuk } =
+         req.body
+      const [{ insertId }] = await connection.promise().query(
+         `INSERT INTO users (nama_pengendara,jenis_kendaraan, no_kendaraan, jam_masuk) 
+           VALUES (?, ?,?)`,
+         [nama_pengendara, jenis_kendaraan, no_kendaraan, jam_masuk]
+      )
+      res.status(202).json({
+         message: 'User Created',
+      })
+   } catch (err) {
+      res.status(500).json({
+         message: err,
+      })
+   }
+})
+
+app.get('/pengendara', async (req, res) => {
+   try {
+      const data = await connection
+         .promise()
+         .query(`SELECT *  from data_pengendara;`)
+      res.status(202).json({
+         pengendara: data[0],
+      })
+   } catch (err) {
+      res.status(500).json({
+         message: err,
+      })
+   }
 })
 
 app.listen(PORT, () => {
